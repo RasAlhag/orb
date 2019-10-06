bd19d6ef71bf429502b7297224706df9d83e255e
greabock <greabock@gmail.com>
2019-10-07 00:53:22 +0300

    change app layout from html to pug

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,27 @@
-<template>
-  <v-app>
-    <v-navigation-drawer app>
-      <!-- -->
-    </v-navigation-drawer>
-
-    <v-app-bar app>
-      <!-- -->
-    </v-app-bar>
-
-    <!-- Sizes your content based upon application components -->
-    <v-content>
-      <!-- Provides the application the proper gutter -->
-      <v-container fluid>
-        <router-view />
-      </v-container>
-    </v-content>
-
-    <v-footer app>
-      <!-- -->
-    </v-footer>
-  </v-app>
+<template lang="pug">
+  div
+    v-app()
+      v-navigation-drawer(app, clipped, permanent)
+      v-app-bar(app, dense, clipped-left, height="28", color="grey darken-4").elevation-1
+        v-flex(style="-webkit-app-region: drag").fill-height
+        v-flex(shrink)
+          v-icon(small @click="exit") mdi-close
+      v-content
+        v-container(fluid)
+          router-view
+      v-footer(app)
 </template>
 
 <script>
-
-
-export default {
-  name: 'App',
-  components: {
-
+  export default {
+    name: 'App',
+    components: {},
+    methods: {
+      exit() {
+        window.close()
+      }
+    }
   }
-}
 </script>
 
 <style>
