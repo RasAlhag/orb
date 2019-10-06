116cd0e0323438d9e02c3fced1978740f9dab682
greabock <greabock@gmail.com>
2019-10-06 23:20:20 +0300

    Fixed initial

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,32 @@
 <template>
-    <v-app>
-        <v-navigation-drawer app>
-            <!-- -->
-        </v-navigation-drawer>
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
 
-        <v-app-bar app>
-            <!-- -->
-        </v-app-bar>
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar>
 
-        <!-- Sizes your content based upon application components -->
-        <v-content>
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-content>
 
-            <!-- Provides the application the proper gutter -->
-            <v-container fluid>
-                <router-view>
-
-
-
-
-                </router-view>
-            </v-container>
-        </v-content>
-
-        <v-footer app>
-            <!-- -->
-        </v-footer>
-    </v-app>
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
 
   }
